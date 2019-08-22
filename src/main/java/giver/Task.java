9d08f02276ac7a4ae57e5c13c7490f4a70db893e
ToDo <Stelmakhandrew@gmail.com>
2019-08-22 19:17:10 +0300

    init

--- src/main/java/giver/Task.java
+++ src/main/java/giver/Task.java
@@ -7,26 +7,6 @@ import java.util.List;
 
 public class Task {
 
-    public static void main(String[] args) {
-        List<Person> persons = Arrays.asList(
-                new Person( 5  , "Мухомор"),
-                new Person( 6  , "Доброжир "),
-                new Person( 7  , "Иркаил "),
-                new Person( 8  , "Серафим"),
-                new Person( 9  , "Агниил "),
-                new Person( 15 , "Мерилл "),
-                new Person( 14 , "Агафий "),
-                new Person( 13 , "Рёрик  "),
-                new Person( 50 , "Святополк"),
-                new Person( 2  , "Баркадий"),
-                new Person( 1  , "Аркадий")
-        );
-
-        int ids[] = new int[] {10,1,12,13,14,15};
-
-        order(persons, ids);
-        System.out.println(persons);
-    }
 // З List операції заміни та перестановки були б дуже затратними, тому вирішив створити новий LinkedList.
 // Проходжу циклом по даному List<Person>. Якщо id не вказано то елемент додається вкінець LinkedList,
 // якщо вказано - на відповідне місце. У програмі враховується що id приймає унікальні значення.
